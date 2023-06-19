92ad7096b9d8fbf05dd0e5c9b1d8d0eeefb35f5d
hizmailovich <theannalios@gmail.com>
2023-06-19 17:34:50 +0300

    Checkstyle fixed

--- src/main/java/com/solvd/blog/serializer/ErrorSerializer.java
+++ src/main/java/com/solvd/blog/serializer/ErrorSerializer.java
@@ -12,7 +12,8 @@ public class ErrorSerializer extends JsonSerializer<JsonError> {
     @Override
     public void serialize(final JsonError value,
                           final JsonGenerator gen,
-                          final SerializerProvider serializers) throws IOException {
+                          final SerializerProvider serializers
+    ) throws IOException {
         gen.writeStartObject();
         gen.writeStringField("message", value.message());
         gen.writeEndObject();
